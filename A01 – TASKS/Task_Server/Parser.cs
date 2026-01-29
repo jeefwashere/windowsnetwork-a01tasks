8e1535ed0c5d182e681fd805bb1298040b2cf21f
Jeff Tieng <jtieng1377@conestogac.on.ca>
2026-01-29 11:24:22 -0500

    Create file size parser

---
+++ A01 – TASKS/Task_Server/Parser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Server
+{
+    internal class Parser
+    {
+        public double ParseFileSizeMessage(string message)
+        {
+            double response = 0.00;
+            string[] parts = message.Split(":");
+            bool valid = false;
+
+            if (parts.Length != 2 && parts[0] != "FILE SIZE")
+            {
+                response = -1;
+                valid = true;
+            }
+
+            if (valid)
+            {
+                if (!double.TryParse(parts[1], out double fileSize))
+                {
+                    response = -1.00;
+                    valid = false;
+                }
+
+                response = fileSize;
+            }
+
+            return response;
+        }
+    }
+}
