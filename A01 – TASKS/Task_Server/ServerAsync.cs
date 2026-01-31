718431e12e584076f727c3a0bad576af0f8aee69
Jeff Tieng <jtieng1377@conestogac.on.ca>
2026-01-31 01:16:03 -0500

    Changed stream read and write operations to async, changed volatile flag to cancellation token. Need attention to some comments asking questions

--- A01 – TASKS/Task_Server/ServerAsync.cs
+++ A01 – TASKS/Task_Server/ServerAsync.cs
@@ -1,6 +1,17 @@
-﻿using A01___TASKS;
+﻿//
+// FILE               : Server.cs
+// PROJECT            : A01 - TASKS
+// PROGRAMMER		  : Josiah Williams, Ricardo Gao, Jeff David Tieng
+// FIRST VERSION      : 2025-01-28
+// DESCRIPTION        : This file is where the server establish a connection to the client
+// 
+// Name               : Server            
+// Purpose            : The server will establish a connection to the client using the TCP/IP reading the 
+//                      IP and port from a config file.
+using A01___TASKS;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -21,17 +32,46 @@ namespace Task_Server
         Parser parser = new Parser();
         MessageProcessor processor = new MessageProcessor();
 
-        public async Task GetClientConnection()
+        public async Task RunAsync(CancellationToken cancellationToken)
+        {
+            
+            GetConfigInfo();
+            await GetClientConnectionAsync(cancellationToken);
+        }
+
+        public void GetConfigInfo()
+        {
+            string address = ConfigurationManager.AppSettings["IPAddress"] ?? "127.0.0.1";
+            validIP = validator.ValidateIp(address); //happy path works 
+
+            if (validIP == null)
+            {
+                // log invalid IP address
+                // Throw exception maybe?
+            }
+
+            validPort = validator.ValudatePort();
+            if (validPort == KInvalidPort)
+            {
+                //logger invalidport
+            }
+            validServerFileName = ConfigurationManager.AppSettings["ServerFileName"] ?? string.Empty;
+            validLoggerName = ConfigurationManager.AppSettings["LoggerFileName"] ?? string.Empty;
+
+            Console.WriteLine($"[SERVER CONFIG] IP={validIP} PORT={validPort}");
+        }
+
+        public async Task GetClientConnectionAsync(CancellationToken cancellationToken)
         {
             TcpListener listener = new TcpListener(validIP, validPort);
             listener.Start();
-            while (run)
+            while (!cancellationToken.IsCancellationRequested)
             {
                 try
                 {
                     //Create a listener client
                     TcpClient client = await listener.AcceptTcpClientAsync();
-                    await ProcessRequest(client);
+                    await ProcessRequest(client); // Check if concurrency works
                 }
                 catch (SocketException ex)
                 {
