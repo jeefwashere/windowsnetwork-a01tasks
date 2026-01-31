b7d46e11332e4af233918989e3d966df9c2a1d66
Jeff Tieng <jtieng1377@conestogac.on.ca>
2026-01-31 00:20:54 -0500

    Fix commit to push Async Server method

---
+++ A01 – TASKS/Task_Server/ServerAsync.cs
@@ -0,0 +1,80 @@
+﻿using A01___TASKS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Server
+{
+    class ServerAsync
+    {
+        int KInvalidPort = -1;
+        IPAddress validIP;
+        int validPort;
+        string validServerFileName;
+        string validLoggerName;
+        double fileSize;
+        ValidationClass validator = new ValidationClass();
+        Parser parser = new Parser();
+        MessageProcessor processor = new MessageProcessor();
+
+        public async Task ProcessRequest(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+
+            // Data buffer
+            byte[] data = new byte[1024];
+            string incomingData = "";
+            string response = "";
+            int count = 0;
+
+            try
+            {
+                count = stream.Read(data, 0, data.Length);
+                if (count > 0)
+                {
+                    incomingData = Encoding.ASCII.GetString(data, 0, count);
+                    //Logger.Log("Received data: " + incomingData);
+
+                    if (incomingData.StartsWith("FILESIZE"))
+                    {
+                        fileSize = parser.ParseFileSizeMessage(incomingData);
+                    }
+                    else
+                    {
+                        await processor.CheckFile(incomingData, validServerFileName, validLoggerName, fileSize);
+
+                    }
+                    response = "ack";
+                    if (response.Length > 0)
+                    {
+                        byte[] responseBytes = Encoding.ASCII.GetBytes(response);
+                        stream.Write(responseBytes, 0, responseBytes.Length);
+
+                        //Logger.Log($"Response sent: {response}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //Logger.Log($"Error in ProcessRequest: {ex}");
+            }
+            finally
+            {
+                // Cleanup
+                if (stream != null)
+                {
+                    stream.Close();
+                }
+
+                if (client != null)
+                {
+                    client.Close();
+                }
+            }
+        }
+    }
+}
