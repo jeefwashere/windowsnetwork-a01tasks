a1f7da42a459151ef199d8855b3f30aab869a1e0
Jeff Tieng <jtieng1377@conestogac.on.ca>
2026-01-31 18:23:51 -0500

    Applied fixes to client count and message parsing ('\n') recommit

---
+++ A01 – TASKS/A01 – TASKS/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A01___TASKS
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ClientConnector cc = new ClientConnector();
+            await cc.RunAsync();
+
+            Console.WriteLine("Done. Press any key to exit...");
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ A01 – TASKS/Task_Server/Metrics.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Server
+{
+    internal class Metrics
+    {
+        // Wrote this function to measure time
+        public async Task MeasureFileWriteTime(int clientCount, int messageSize, int bufferSize, Stopwatch totalWriteTime, string filePath, long maxFileSize, string logFilePath)
+        {
+            bool sizeReached = false;
+            totalWriteTime.Restart();
+
+            if (string.IsNullOrEmpty(logFilePath))
+            {
+                logFilePath = "metrics.log";
+            }
+
+            while (!sizeReached)
+            {
+                FileInfo fileInfo = new FileInfo(filePath);
+                if (fileInfo.Exists && fileInfo.Length >= maxFileSize)
+                {
+                    sizeReached = true;
+                }
+
+                await Task.Delay(10);
+            }
+
+            totalWriteTime.Stop();
+            await Logger.WriteLoggerAsync(
+                @$"[TIME TO WRITE TO MAX FILE SIZE]
+                Clients: {clientCount}
+                TimeMs: {totalWriteTime.ElapsedMilliseconds}
+                Max File Size: {maxFileSize}
+                Message Size: {messageSize}
+                Buffer Size: {bufferSize}", logFilePath);
+        }
+    }
+}
