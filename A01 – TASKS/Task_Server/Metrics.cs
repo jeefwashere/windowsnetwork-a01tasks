fd3623f404f47d64889592bc4c0ac731bdc53cc3
Jeff Tieng <jtieng1377@conestogac.on.ca>
2026-02-01 18:17:26 -0500

    Comments

--- A01 – TASKS/Task_Server/Metrics.cs
+++ A01 – TASKS/Task_Server/Metrics.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿//
+// FILE               : Metrics.cs
+// PROJECT            : A01 - TASKS
+// PROGRAMMER		  : Josiah Williams, Ricardo Gao, Jeff David Tieng
+// FIRST VERSION      : 2025-01-28
+// DESCRIPTION        : Used to stash metrics entries into a concurrent list
+//
+// Name               : Metrics.cs            
+// Purpose            : The class where each entry is added into a list
+//  
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -12,11 +22,21 @@ namespace Task_Server
     {
         private readonly ConcurrentBag<MetricRecord> metricsRecords = new ConcurrentBag<MetricRecord>(); // Found the thread-safe collections here: https://learn.microsoft.com/en-us/dotnet/standard/collections/thread-safe/
 
+        /// <summary>
+        /// A method to add metrics entries to a list
+        /// </summary>
+        /// <param name="metrics">Each entry of a metric</param>
+        /// <returns>Adds metrics to list</returns>
         public void Record(MetricRecord metrics)
         {
             metricsRecords.Add(metrics);
         }
 
+        /// <summary>
+        /// Returns all metrics in the list
+        /// </summary>
+        /// <param name="metrics">Each entry of a metric</param>
+        /// <returns>All metrics entries</returns>
         public List<MetricRecord> GetMetrics()
         {
             return metricsRecords.ToList();
