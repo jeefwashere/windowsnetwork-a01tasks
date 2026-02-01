b539713833a4ac790ac3274a082fb1bde63b45a1
Jeff Tieng <jtieng1377@conestogac.on.ca>
2026-02-01 18:18:12 -0500

    Comments

---
+++ A01 – TASKS/Task_Server/MetricRecord.cs
@@ -0,0 +1,39 @@
+﻿//
+// FILE               : MetricRecord.cs
+// PROJECT            : A01 - TASKS
+// PROGRAMMER		  : Josiah Williams, Ricardo Gao, Jeff David Tieng
+// FIRST VERSION      : 2025-01-28
+// DESCRIPTION        : Represents a metric entry
+//
+// Name               : MetricRecord.cs            
+// Purpose            : Represents a metric entry
+//  
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Server
+{
+    internal class MetricRecord
+    {
+        public int ClientCount { get; set; }
+        public int MessageSize { get; set; }
+        public int BufferSize { get; set; }
+        public long MaxFileSize { get; set; }
+        public long WriteTime {  get; set; }
+        public DateTime Timestamp {  get; set; }
+
+        public MetricRecord(int clientCount, int messageSize, int bufferSize, long maxFileSize, long writeLatencyMs, DateTime timestamp)
+        {
+            ClientCount = clientCount;
+            MessageSize = messageSize;
+            BufferSize = bufferSize;
+            MaxFileSize = maxFileSize;
+            WriteTime = writeLatencyMs;
+            Timestamp = timestamp;
+        }
+    }
+}
